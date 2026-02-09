0110db017068a4ae2385cad14153c57afb5f537e
June Smith <smithjun@oregonstate.edu>
2026-02-09 10:57:07 -0800

    In class Challenge 6-1

---
+++ in-class-challenges/challlenge6-1/challenge6-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+//  Alt: make sig_handler
+
+int main(void) {
+
+    struct sigaction sa = {
+        .sa_handler = DFL,
+        .sa_flags = 0
+    }
+
+    if (sigaction(SIGINT, &sa, NULL) == -1){
+        perror("sigaction");
+        exit(1);
+    }
+    sigemptyset(&sa.sa_mask);
+    puts("^C locked out for 5 seconds! Try it!\n");
+    sleep(5);
+    
+    
+    struct sigaction sa = {
+        .sa_handler = SIG_IGN,
+        .sa_flags = 0
+    }
+    sigemptyset(&sa.sa_mask);
+    puts("^C locked out for 5 seconds! Try it!\n");
+    sleep(5);
+    sleep(10);
+    
+}
+
+
