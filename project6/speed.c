516e55ec219b0692ca10768837642c484cc39f4c
June Smith <smithjun@oregonstate.edu>
2026-02-10 23:46:06 -0800

    Empty project 6

---
+++ project6/speed.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    //  commands user can enter: -, +, q
+    
+
+}
+
+
