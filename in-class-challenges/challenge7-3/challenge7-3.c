8a7deeea5d40f885004d5bca2ad0bfb7d519ae45
June Smith <smithjun@oregonstate.edu>
2026-02-18 11:05:31 -0800

    ICC 7-3

--- in-class-challenges/challenge7-3/challenge7-3.c
+++ in-class-challenges/challenge7-3/challenge7-3.c
@@ -7,8 +7,8 @@
 
 int *run(void *args)
 {
-    void(args);
-    puts("running");
+    int *id = args;
+    puts("running from %d\n", id);
     return NULL;
 
 }
@@ -19,9 +19,11 @@ int main(void)
     puts("parent: hello!");
 
     pthread_t thread[THREAD_COUNT];
+    int thread_id[THREAD_COUNT];
+
 
     for (int i = 0; i<THREAD_COUNT; i++) {
-        pthread_create(thread + i, NULL, run, NULL);	// Pointer arithmetic
+        pthread_create(thread + i, NULL, run, thread_id + i);	// Pointer arithmetic
     }
     for (int i = 0; i<THREAD_COUNT; i++) {
         pthread_join(thread[i], NULL);
