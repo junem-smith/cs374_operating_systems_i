cd382f66ebbc80b644fc36dfc1843276bd3eb5e6
June Smith <smithjun@oregonstate.edu>
2026-02-25 10:34:36 -0800

    In Class Challenge 8-3 Deadlocking

---
+++ in-class-challenges/challenge8-3/deadlock.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define ELEMENTS 50
+#define THREADS 8
+
+pthread_mutex_t lock_a = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_b = PTHREAD_MUTEX_INITIALIZER;
+
+int a[ELEMENTS] = {0};
+int b[ELEMENTS] = {0};
+
+void *run1(void *arg)
+{
+    (void)arg;
+
+    for (int i = 0; i < ELEMENTS; i++) {
+        pthread_mutex_lock(&lock_a);
+        pthread_mutex_lock(&lock_b);    // Deadlock
+        a[i]++;
+        pthread_mutex_unlock(&lock_a);
+        pthread_mutex_unlock(&lock_b);
+        
+    }
+
+    return NULL;
+}
+
+void *run2(void *arg)
+{
+    (void)arg;
+
+    for (int i = 0; i < ELEMENTS; i++) {
+        pthread_mutex_lock(&lock_a); 
+        pthread_mutex_lock(&lock_b);    
+           
+        a[i]++;
+        pthread_mutex_unlock(&lock_a);
+        pthread_mutex_unlock(&lock_b);
+        
+    }
+
+    return NULL;
+}
+
+int main(void)
+{
+    pthread_t t1, t2;
+
+    pthread_create(&t1, NULL, run1, NULL);
+    pthread_create(&t2, NULL, run2, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+}
